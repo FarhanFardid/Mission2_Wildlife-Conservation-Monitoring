7fa48e51a2ceef871f5f6737c9089d0ed275ca35
FarhanFardid <farhanfardid009@gmail.com>
2025-05-26 02:55:55 +0600

    sightings data inserted

--- WildlifeConservation.sql
+++ WildlifeConservation.sql
@@ -19,9 +19,9 @@ create TABLE sightings (
     sighting_id SERIAL UNIQUE PRIMARY KEY NOT NULL,
     ranger_id INTEGER REFERENCES  rangers(ranger_id),
     species_id INTEGER REFERENCES species(species_id),
-    sighting_time TIMESTAMP NOT NULL,
     location VARCHAR(50) NOT NULL,
-    notes VARCHAR(50) DEFAULT 'Null'
+    sighting_time TIMESTAMP NOT NULL,
+    notes VARCHAR(50) DEFAULT '(Null)'
 );
 
 -- Inserting rangers data
@@ -37,7 +37,16 @@ INSERT INTO species (common_name,scientific_name,discovery_date,conservation_sta
 ('Red Panda','Ailurus fulgens','1825-01-01','Vulnerable'),
 ('Asiatic Elephant','Elephas maximus indicus','1758-01-01','Endangered');
 
+-- Inserting sightings data
+INSERT INTO sightings (ranger_id ,species_id,location,sighting_time,notes ) VALUES 
+(1,1,'Peak Ridge','2024-05-10 07:45:00','Camera trap image captured'),
+(2,2,'Bankwood Area','2024-05-12 16:20:00','Juvenile seen'),
+(3,3,'Bamboo Grove East','2024-05-15 09:10:00','Feeding observed'),
+(2,1,'Snowfall Pass','2024-05-18 18:30:00', DEFAULT);
+
 
 SELECT * from rangers;
 SELECT * from species;
 SELECT * from sightings;
+
+drop Table sightings;
